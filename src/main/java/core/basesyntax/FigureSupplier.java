6b25d93c7a1fe5decb8a0ac9c74cfb9f790607c6
noleynik29 <noleynik29@gmail.com>
2025-09-03 13:29:15 +0300

    There are some figures of the following types: square, rectangle, right triangle, circle, isosceles trapezoid. I've to created corresponding classes for them(Square, Rectangle, RightTriangle, Circle, IsoscelesTrapezoid) All figures have                                                                - state - all figures have color, but each figure type can also have one or several unique properties (radius for circle, firstLeg and secondLeg for right triangle, and so on).  - behavior - we can obtain the area of any figure and are able to draw it. To 'draw' means to print out all information about a figure using System.out.println() (you shouldn't override the toString() method for this).

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
+import core.basesyntax.model.Circle;
 import core.basesyntax.model.Figure;
-import core.basesyntax.model.Square;
+import core.basesyntax.model.IsoscelesTrapezoid;
 import core.basesyntax.model.Rectangle;
 import core.basesyntax.model.RightTriangle;
-import core.basesyntax.model.Circle;
-import core.basesyntax.model.IsoscelesTrapezoid;
+import core.basesyntax.model.Square;
 import java.util.Random;
 
 public class FigureSupplier {
